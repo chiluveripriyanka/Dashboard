1dc9dcf31160094d8b227bdec479d359cf092a46
sandeep <sandeepnanda2>
2017-11-14 19:59:50 +0530

    NAV header hiding done before login

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
-angular.module('admin_dashboard', ['datatables', 'ngRoute'])
+var app = angular.module('admin_dashboard', ['datatables', 'ngRoute']);
+app.run(['$rootScope', '$route', function ($rootScope, $route) {
+   
+
+    // $rootScope.$watch(function () {
+    //     return sessionStorage.getItem('chIns_ngSession');
+    // }, function (newVal) {
+
+    //     if (newVal === null) {
+    //         $rootScope.userAuth = false;
+    //     } else {
+    //         $rootScope.userAuth = true;
+    //     }
+    // });
+
+    $rootScope.$on('$routeChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+        console.log(toState.originalPath);
+        if(toState.originalPath!='/'){
+            $rootScope.isLoggedin= true;
+        }
+        else{
+            $rootScope.isLoggedin= false;
+        }
+        // if (toState.originalPath != "quotesListing") {
+        //     angular.element(document.body.querySelectorAll(".chIns_bottomSelectedCompaniesWrapperQL")).remove();
+        // }
+
+    });
+
     
-.config(function ($routeProvider) {
+
+    $rootScope.domainNameUrl = window.serverIP;
+
+}]);
+
+app.config(function ($routeProvider) {
     $routeProvider
     .when("/" , {
         templateUrl : "partials/login.html"
@@ -14,23 +47,35 @@ angular.module('admin_dashboard', ['datatables', 'ngRoute'])
     })
     .otherwise({
         redirectTo : "/"
-    })
-})
-.controller('loginController', function($scope, $location) {
-
+    });
+});
+app.controller('loginController', function($scope, $location) {
+console.log($location.$$path);
+if($location.$$path=='/'){
+    $scope.hideHeader = true;
+}
     // function to submit the form after all validation has occurred            
     $scope.submitLoginForm = function() {
 
         // check to make sure the form is completely valid
         if ($scope.loginForm.$valid) {
-            $location.path('/dashboard');
+            if($scope.user.password=='admin@123' && $scope.user.email=='user@digitalrupay.com'){
+                $location.path('/dashboard');
+            }
+            else{
+                $scope.authStatus =true;
+                console.log('auth failed',$scope.user);
+            }
+            
+            
         }
 
     };
 
-})
-.controller('AddCategoryController', function($scope, $location) {
-
+});
+app.controller('AddCategoryController', function($scope, $location) {
+    $scope.hideHeader = 'falsesss';
+    console.log($location.$$path);
     // function to submit the form after all validation has occurred            
     $scope.submitCategoryForm = function() {
         var categoryInfo = $("#addCategoryForm").serialize();
@@ -38,8 +83,9 @@ angular.module('admin_dashboard', ['datatables', 'ngRoute'])
 
     };
 
-})
-.controller('dashboardController', function($scope,$http,DTOptionsBuilder, DTColumnBuilder) {
+});
+app.controller('dashboardController', function($scope,$http,DTOptionsBuilder, DTColumnBuilder) {
+    $scope.hideHeader = false;
     $http.get('https://jsonplaceholder.typicode.com/posts').success(function(data) {
         $scope.userData = data;
         console.log(data);
@@ -48,4 +94,4 @@ angular.module('admin_dashboard', ['datatables', 'ngRoute'])
             $scope.vm.dtOptions = DTOptionsBuilder.newOptions()
               .withOption('order', [0, 'asc']);
     });
-})
+});
