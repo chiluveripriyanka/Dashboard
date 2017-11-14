b0b3aa52918e43e70ca3db0ee765236e8374e66b
priyanka <priyanka@sureify.com>
2017-11-15 00:09:11 +0530

    add categories

--- js/app.js
+++ js/app.js
@@ -45,6 +45,9 @@ app.config(function ($routeProvider) {
     .when("/add_edit_categories",{
         templateUrl : "partials/add_edit_categories.html"
     })
+    .when("/add_edit_sub_categories",{
+        templateUrl : "partials/add_edit_sub_categories.html"  
+    })
     .otherwise({
         redirectTo : "/"
     });
@@ -73,17 +76,7 @@ if($location.$$path=='/'){
     };
 
 });
-app.controller('AddCategoryController', function($scope, $location) {
-    $scope.hideHeader = 'falsesss';
-    console.log($location.$$path);
-    // function to submit the form after all validation has occurred            
-    $scope.submitCategoryForm = function() {
-        var categoryInfo = $("#addCategoryForm").serialize();
-        console.log(categoryInfo);
 
-    };
-
-});
 app.controller('dashboardController', function($scope,$http,DTOptionsBuilder, DTColumnBuilder) {
     $scope.hideHeader = false;
     $http.get('https://jsonplaceholder.typicode.com/posts').success(function(data) {
@@ -95,3 +88,77 @@ app.controller('dashboardController', function($scope,$http,DTOptionsBuilder, DT
               .withOption('order', [0, 'asc']);
     });
 });
+
+app.controller('AddCategoryController', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+    $scope.uploadPic = function(file) {
+        file.upload = Upload.upload({
+          url: 'http://ec2-54-88-194-105.compute-1.amazonaws.com:3000/add_category',
+          data: {category_name: $scope.category_name, file: file},
+        });
+        file.upload.then(function (response) {
+          $timeout(function () {
+            file.result = response.data;
+          });
+          if(response.data.status == 'true'){
+            swal({
+                title: "Here's a message!",
+                type: "success",
+                text: response.data.message,
+                confirmButtonText : "Close this window"
+            });
+          }else{
+            swal({
+                title: "Here's a message!",
+                type: "warning",
+                text: response.data.message,
+                confirmButtonText : "Close this window"
+            });
+          }
+        }
+        // , function (response) {
+        //   if (response.status > 0)
+        //     $scope.errorMsg = response.status + ': ' + response.data;
+        // }, function (evt) {
+        //   // Math.min is to fix IE which reports 200% sometimes
+        //   file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        // }
+        );
+    }
+}]);
+app.controller('AddSubCategoryController', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+    $scope.uploadPic = function(file) {
+        file.upload = Upload.upload({
+          url: 'http://ec2-54-88-194-105.compute-1.amazonaws.com:3000/add_sub_category',
+          data: {sub_category_name: $scope.sub_category_name, cat_id: $scope.cat_id, file: file},
+        });
+        file.upload.then(function (response) {
+            console.log(response);
+          $timeout(function () {
+            file.result = response.data;
+          });
+          if(response.data.status == 'true'){
+            swal({
+                title: "Here's a message!",
+                type: "success",
+                text: response.data.message,
+                confirmButtonText : "Close this window"
+            });
+          }else{
+            swal({
+                title: "Here's a message!",
+                type: "warning",
+                text: response.data.message,
+                confirmButtonText : "Close this window"
+            });
+          }
+        }
+        // , function (response) {
+        //   if (response.status > 0)
+        //     $scope.errorMsg = response.status + ': ' + response.data;
+        // }, function (evt) {
+        //   // Math.min is to fix IE which reports 200% sometimes
+        //   file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+        // }
+        );
+    }
+}]);
